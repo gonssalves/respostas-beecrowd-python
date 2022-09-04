ce2253145d8edd347293563014b62f32e6197fe6
Vinícius Gonçalves <103079267+gonssalves@users.noreply.github.com>
2022-09-03 21:57:09 -0300

    Create 1060.py
    Números positivos

---
+++ 1060.py
@@ -0,0 +1,30 @@
+l = []
+
+a = float(input())
+
+if a > 0:
+    l.append(a)
+
+b = float(input())
+if b > 0:
+    l.append(b)
+
+c = float(input())
+if c > 0:
+    l.append(c)
+
+d = float(input())
+if d > 0:
+    l.append(d)
+
+e = float(input())
+if e > 0:
+    l.append(e)
+
+f = float(input())
+if f > 0:
+    l.append(f)
+
+
+x = len(l)
+print(f"{x} valores positivos")
